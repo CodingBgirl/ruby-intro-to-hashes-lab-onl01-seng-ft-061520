d0a6e113969b5bc9b96673a6cfef5a5a22755aae
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-06-28 23:11:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -47,9 +47,9 @@ def monopoly_with_fourth_tier
 		railroads: {
 			pieces: 4,
 			names: {
-				:reading_railroad => {}, 
-        :pennsylvania_railroad => {}, 
-        :b_and_o_railroad => {}, 
+				:reading_railroad => {},
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
         :shortline => {}
 			},
 			rent_in_dollars:{
